15b5c76830f3f486b9f65ddaf5446b34558f0a35
Nurbolot <ngulamidinov@gmail.com>
2022-11-15 18:56:27 +0600

    update car Controller delete logics

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,41 +5,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.Cars;
+import web.service.CarService;
 
 import java.util.Optional;
 
 @Controller
 public class CarController {
-    private final Cars cars;
+    private final CarService carService;
 
     @Autowired
-    public CarController(Cars cars) {
-        this.cars = cars;
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
     @GetMapping("/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer count, ModelMap modelMap) {
-        StringBuilder result = new StringBuilder();
         Optional<Integer> optional = Optional.ofNullable(count);
         if (optional.isPresent()) {
-            result = getCars(count);
+            modelMap.addAttribute("mes", carService.getCarList(count));
         } else {
-            result = getCars();
+            modelMap.addAttribute("mes", carService.getCarList());
         }
-        modelMap.addAttribute("mes", result);
+
         return "cars";
-    }
-
-    private StringBuilder getCars() {
-        return getCars(cars.size());
-    }
-
-    private StringBuilder getCars(int count) {
-        StringBuilder result = new StringBuilder(count);
-        for (int i = 0; i < cars.size() && i < count; i++) {
-            result.append(cars.getCarByIndex(i)).append("\n");
-        }
-        return result;
     }
 }
